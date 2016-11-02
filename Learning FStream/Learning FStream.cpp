89fd759a061a96b8f4d1f052a81b20f125478582
Covton <Covton@COVTON-PC>
2016-11-02 20:47:08 +0000

    Breaking on multiple lines

--- Learning FStream/Learning FStream.cpp
+++ Learning FStream/Learning FStream.cpp
@@ -6,11 +6,48 @@
 #include <fstream>
 #include <sstream>
 
+//global const of file name
+const std::string myfile = "Input.txt";
+
+std::string ReadFileIntoString(std::string filename)
+{
+	//define an input file stream called ReadFile
+	std::fstream ReadFile;
+
+	//open the file into the stream
+	ReadFile.open(filename);
+
+	//define a string to hold the final and each temporary output
+	std::string strResult;
+	std::string strTemp;
+
+	while (!ReadFile.eof())
+	{
+		std::getline(ReadFile, strTemp);
+		if (strResult.empty())
+		{
+			strResult = strTemp;
+		}
+		else
+		{
+			strResult = strResult + "\n" + strTemp;
+		}
+	}
+	ReadFile.close();
+
+	return strResult;
+}
+
+
+
 int main()
 {
-    
-	
-	
+	std::string strFile;
+
+	strFile = ReadFileIntoString(myfile);
+	std::cout << strFile << "\n";
+
+	system("PAUSE");
 	
 	return 0;
 }
